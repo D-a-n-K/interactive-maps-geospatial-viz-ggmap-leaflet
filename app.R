ce39397c8a7efe77560f9436da4b2f96a75d3163
Sara Huta <sh5963a@american.edu>
2026-03-15 17:16:57 -0400

    deliverables 36-38

--- app.R
+++ app.R
@@ -4,7 +4,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Sara's Shiny App"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
